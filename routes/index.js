9d987314956fa3781c3f982e7cc69734ef0ca29c
nonnhjleno <taaaisei999@gmail.com>
2023-10-11 16:42:43 +0900

    データベース一覧を取得するAPI作成

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const con = require('./userInfo').con;
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+let database = {};
+
+con.connect((err) => {
+  if (err) throw err;
+  console.log('Connected');
+  const sql = 'show databases;';
+  con.query(sql, (err, result, fields) => {
+    if (err) throw err;
+    database = result;
+  });
 });
+
+router
+  .get('/', (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000/');
+    res.send(database);
+  });
 
 module.exports = router;
