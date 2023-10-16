7fa7a64ed775a89d2a0004a84fbe89cd52c3ac73
nonnhjleno <taaaisei999@gmail.com>
2023-10-16 12:17:56 +0900

    テーブル一覧を返すAPIをpoolを使わずに作成

--- routes/fetchTables.js
+++ routes/fetchTables.js
@@ -1,21 +1,28 @@
+const userInfo = require('./userInfo').userInfo;
+
 const fetchTableNames = async (databaseName) => {
     return new Promise((resolve, reject) => {
-        const pool = require('./userInfo').pool;
-        pool.config.connectionConfig.database = databaseName;
-        pool.getConnection((err, connection) => {
+        const mysql = require('mysql');
+        const con = mysql.createConnection({
+            ...userInfo,
+            database: databaseName
+        });
+        con.connect((err) => {
             if (err) throw err;
-            connection.query("show tables", (error, results) => {
+            console.log('Connected to the database');
+            con.query("SHOW TABLES", (error, results) => {
+                con.end();
                 if (error) {
                     reject(error);
                 } else {
                     const tableNames = results.map((row) => Object.values(row)[0]);
-                    pool.config.connectionConfig.database = null;
                     resolve(tableNames);
                 }
             });
         });
     });
 }
+
 
 module.exports = {
     fetchTableNames,
